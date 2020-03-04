2547bad4880ae701a6c4303572b4d6e2462500b8
Thomas Argent <tfargent@gmail.com>
2020-03-04 17:11:00 -0500

    fixed search!

--- employee-directory/src/pages/home.js
+++ employee-directory/src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 import Container from "../components/Container";
 import Alert from "../components/Alert";
@@ -7,93 +7,127 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 import SearchForm from "../components/SearchForm";
 
-class Employees extends Component {
+function Employees() {
+    const [search, setSearch] = useState("");
+    const [employees, setEmployees] = useState([]);
+    const [results, setResults] = useState([]);
+    const [error, setError] = useState("");
 
-    state = {
-        search: "",
-        employees: [],
-        results: [],
-        error: ""
+    useEffect(() => {
+        API.getEmployees()
+            .then(res => {
+                setEmployees(res.data.results);
+                setResults(res.data.results);
+            })
+            .catch(err => setError(err));
+    }, []);
+
+    useEffect(() => {
+
+        if( !search){
+            return
+        }
+
+        var text= search.toLocaleLowerCase();
+        var filtered = employees.filter(function(data){
+            return data.name.first.toLowerCase().includes(text)
+        });
+        setResults(filtered);
+        console.log(text)
+
+    }, [search]);
+
+    // state = {
+    //     search: "",
+    //     employees: [],
+    //     results: [],
+    //     error: ""
+    // };
+
+    // componentDidMount() {
+    //         API.getEmployees()
+    //         // .then(res => console.log(res.data.results))
+    //         .then(res => this.setState({ employees: res.data.results, results: res.data.results }))
+
+    //         .catch(err => console.log(err));
+
+    // };
+
+    // handleInputChange = event => {
+    //     // search state seems to be behind by one keypress at all times. 
+
+
+
+    //     this.setState({
+    //         search: event.target.value,
+    //         results: this.state.employees
+    //     }, () => { console.log(search) });
+
+    //     var search = this.state.search
+    //     var filtered = this.state.employees.filter(function (name) {
+    //         return name.name.first.includes(search)
+    //     })
+    //     this.setState({ results: filtered })
+    //     console.log(filtered)
+
+
+
+    // // };
+
+
+    const handleInputChange = event => {
+        setSearch(event.target.value);
     };
 
-    componentDidMount() {
-            API.getEmployees()
-            // .then(res => console.log(res.data.results))
-            .then(res => this.setState({ employees: res.data.results, results: res.data.results }))
-            
-            .catch(err => console.log(err));
-            
-    };
 
-    handleInputChange = event => {
-        // search state seems to be behind by one keypress at all times. 
-        this.setState({ 
-            search: event.target.value, 
-            results: this.state.employees }, () => {console.log(search)});
-        
-        var search = this.state.search
-        var filtered = this.state.employees.filter(function(name){
-            return name.name.first.includes(search)
-        })
-        this.setState({results: filtered})
-        console.log(filtered)
-      
-        
+    return (
+        <div>
+            <Container style={{ minHeight: "80%" }}>
+                <h1 className="text-center">Employee Directory</h1>
+                <Alert
+                    type="danger"
+                    style={{ opacity: error ? 1 : 0, marginBottom: 10 }}
+                >
+                    {error}
+                </Alert>
 
-    };
+                <Row>
+                    <Col />
+                    <Col size="sm-4">
+                        <SearchForm
+                            handleInputChange={handleInputChange}
 
-
-
-
-    render() {
-        return (
-            <div>
-                <Container style={{ minHeight: "80%" }}>
-                    <h1 className="text-center">Employee Directory</h1>
-                    <Alert
-                        type="danger"
-                        style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-                    >
-                        {this.state.error}
-                    </Alert>
-
-                    <Row>
-                        <Col />
-                        <Col size="sm-4">
-                        <SearchForm 
-                        handleInputChange={this.handleInputChange}
-                        
                         />
-                            
-                        
-                        </Col>
-                        <Col />
-                    </Row>
-
-                    <Row>
-                        <Col >
-                            <h4>Image</h4>
-                        </Col>
-                        <Col >
-                            <h4>Name</h4>
-                        </Col>
-                        <Col >
-                            <h4>Phone</h4>
-                        </Col>
-                        <Col >
-                            <h4>Email</h4>
-                        </Col>
-                        <Col >
-                            <h4>DOB</h4>
-                        </Col>
-                    </Row>
 
 
-                    <EmployeeInfo employees={this.state.results} />
-                </Container>
-            </div>
-        );
-    }
+                    </Col>
+                    <Col />
+                </Row>
+
+                <Row>
+                    <Col >
+                        <h4>Image</h4>
+                    </Col>
+                    <Col >
+                        <h4>Name</h4>
+                    </Col>
+                    <Col >
+                        <h4>Phone</h4>
+                    </Col>
+                    <Col >
+                        <h4>Email</h4>
+                    </Col>
+                    <Col >
+                        <h4>DOB</h4>
+                    </Col>
+                </Row>
+
+
+                <EmployeeInfo employees={results} />
+            </Container>
+        </div>
+    );
+
 
 
 }
