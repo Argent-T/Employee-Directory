f5d5b0cb6ae11ad7d76c1621bab98e318c295395
Thomas Argent <tfargent@gmail.com>
2020-03-07 20:24:48 -0500

    complete

--- employee-directory/src/components/EmployeeInfo/index.js
+++ employee-directory/src/components/EmployeeInfo/index.js
@@ -15,6 +15,9 @@ function bdate(date) {
 function EmployeeInfo(props) {
     const [alpha, setAlpha] = useState(true);
     const [order, setOrder] = useState(true);
+    const [EmailAlpha, setEmailAlpha] = useState(true);
+    const [old, setOld] = useState(true);
+
     const sortHandler = event => {
 
         var sorted
@@ -71,6 +74,59 @@ function EmployeeInfo(props) {
             }
         }
 
+        if(event === "email"){
+            if (!EmailAlpha) {
+                sorted = props.employees.sort(function (a, b) {
+                    if (a.name.first > b.name.first) {
+                        return -1;
+                    }
+                    if (b.name.first > a.name.first) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                setEmailAlpha(true);
+
+                props.change(sorted);
+            }
+            if (EmailAlpha) {
+                sorted = props.employees.sort(function (a, b) {
+                    if (a.email < b.email) {
+                        return -1;
+                    }
+                    if (b.email < a.email) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                setEmailAlpha(false);
+
+                props.change(sorted);
+            }
+
+        }
+        if(event === "dob"){
+            if(!old){
+               sorted = props.employees.sort(function (a, b) {
+
+                    return Date.parse(a.dob.date) - Date.parse(b.dob.date)
+                });
+                setOld(true);
+               
+                props.change(sorted)
+            }
+            if(old){
+                sorted = props.employees.sort(function (a, b) {
+ 
+                     return Date.parse(b.dob.date) - Date.parse(a.dob.date)
+                 });
+                 setOld(false);
+                
+                 props.change(sorted)
+             }
+
+        }
+
 
     }
 
@@ -83,8 +139,8 @@ function EmployeeInfo(props) {
                     <th scope="col"> Image </th>
                     <th className="point" scope="col" onClick={() => sortHandler("name")}> Name </th>
                     <th className="point" scope="col" onClick={() => sortHandler("phone")}> Phone </th>
-                    <th className="point" scope="col"> Email </th>
-                    <th className="point" scope="col"> DOB </th>
+                    <th className="point" scope="col" onClick={() => sortHandler("email")}> Email </th>
+                    <th className="point" scope="col" onClick={() => sortHandler("dob")}> DOB </th>
                 </tr>
             </thead>
             <tbody>
